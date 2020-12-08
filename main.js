f10b2bbae6561834e188ab8883735bc9c40b597f
Ahmad Habibzad <66021219+Habibzad@users.noreply.github.com>
2020-12-07 21:20:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded',function(event){
     var dataText = [ 
 
-        'Be the change that you wish to see in the world. "Mahatma Gandhi"', 
-        'Twenty years from now you will be more disappointed by the things that you didn\'t do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover. "H. Jackson Brown Jr."',
-        'A friend is someone who knows all about you and still loves you. "Elbert Hubbard"', 
-        'You\'ve gotta dance like there\'s nobody watching, Love like you\'ll never be hurt, Sing like there\'s nobody listening, And live like it\'s heaven on earth. "William W. Purkey"',
-        'To live is the rarest thing in the world. Most people exist, that is all. "Oscar Wilde"',
-        'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment. "Ralph Waldo Emerson"' 
+        '"Be the change that you wish to see in the world". \n Mahatma Gandhi', 
+        '"Twenty years from now you will be more disappointed by the things that you didn\'t do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover. "H. Jackson Brown Jr."',
+        '"A friend is someone who knows all about you and still loves you." Elbert Hubbard', 
+        '"You\'ve gotta dance like there\'s nobody watching, Love like you\'ll never be hurt, Sing like there\'s nobody listening, And live like it\'s heaven on earth". William W. Purkey',
+        '"To live is the rarest thing in the world. Most people exist, that is all." Oscar Wilde',
+        '"To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment." Ralph Waldo Emerson'  
     ]
 
     function typeWriter(text, i, fnCallback) {
